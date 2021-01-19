8b6bbcabb845f3a4ad39c0090479ee325b73bb2b
cristianoDevSystem <cristianodevsystem@gmail.com>
2021-01-18 22:35:07 -0300

    Update main.dart
    Criando o formulário de login, com email e/o número de telefone e senha, assim como o esqueceu a senha

--- lib/main.dart
+++ lib/main.dart
@@ -82,6 +82,85 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             ),
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Color.fromRGBO(143, 148, 251, .2),
+                              blurRadius: 20.0,
+                              offset: Offset(0, 10))
+                        ]),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(8.0),
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(color: Colors.grey[100]),
+                            ),
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "E-mail ou Número Telefone",
+                                hintStyle: TextStyle(color: Colors.grey[400])),
+                          ),
+                        ),  
+                        Container(
+                          padding: EdgeInsets.all(8.0),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Senha",
+                                hintStyle: TextStyle(color: Colors.grey[400])),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      gradient: LinearGradient(
+                        colors: [
+                          Color.fromRGBO(143, 148, 251, 1),
+                          Color.fromRGBO(143, 148, 251, .6),
+                        ]
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Entrar",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 70,
+                  ),
+                  Text(
+                        "Esqueceu a senha?",
+                        style: TextStyle(
+                          color: Color.fromRGBO(143, 148, 251, 1),
+                        ),
+                      ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
